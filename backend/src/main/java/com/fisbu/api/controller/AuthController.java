4927ad1de28b16fb4856350b3867462699b9f013
barishansu45 <25070006080@stu.yasar.edu.tr>
2026-06-13 22:32:11 +0300

    feat: login endpoint ve JWT token üretimi eklendi

--- backend/src/main/java/com/fisbu/api/controller/AuthController.java
+++ backend/src/main/java/com/fisbu/api/controller/AuthController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.fisbu.api.dto.LoginRequest;
+import com.fisbu.api.dto.AuthResponse;
 import com.fisbu.api.dto.RegisterRequest;
 import com.fisbu.api.entity.User;
 import com.fisbu.api.service.AuthService;
@@ -22,5 +23,11 @@ public class AuthController {
     @PostMapping("/register") // HTTP POST isteği ile "/auth/register" endpoint'ine gelen kayıt isteklerini işler.
     public User register(@RequestBody RegisterRequest request) {
         return authService.register(request);
+    }
+
+    @PostMapping("/login")// HTTP POST isteği ile "/auth/login" endpoint'ine gelen giriş isteklerini işler.
+    public AuthResponse login(@RequestBody LoginRequest request) {//Gelen JSON'ı LoginRequest nesnesine dönüştürür ve authService.login() metodunu çağırarak kullanıcı giriş işlemini gerçekleştirir. Giriş başarılı ise bir token döndürür.
+        String token = authService.login(request);
+        return new AuthResponse(token);
     }
 }
---
+++ backend/src/main/java/com/fisbu/api/dto/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.fisbu.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
